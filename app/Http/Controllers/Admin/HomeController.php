0ea5d852fddd61267001f2d2e0eb0beaba70047f
felixrp74 <felixrp74@gmail.com>
2023-04-03 19:10:16 -0500

    estadisticas del sistema menu principal

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Estudiante;
+use App\Models\Docente;
+use App\Models\Apoderado;
+use App\Models\Curso;
+
 class HomeController extends Controller
 {
     public function index(){
 
+        $estudiantes = Estudiante::all();
+        $docentes = Docente::all();
+        $cursos = Docente::all();
+        $apoderados = Apoderado::all();
+        $cursos = Curso::all();
         
-        
-        return view('admin.index');
+        return view('admin.index', compact('estudiantes', 'docentes', 'apoderados', 'cursos'));
     }
     
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,8 +6,94 @@
     <h1>sistemas</h1>
 @stop
 
-@section('content')
-    <h1>Bienvenido</h1>
+@section('content')     
+
+
+
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Bienvenido</h1>
+                </div>
+                <div class="col-sm-6">
+                    {{-- <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard v1</li>
+                    </ol> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-lg-3 col-6">
+
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{$estudiantes->count()}}</h3>
+                            <p>Estudiantes</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{$docentes->count()}}</h3>
+                            <p>Docentes</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person-add"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{$apoderados->count()}}</h3>
+                            <p>Apoderados</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-pie-graph"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{$cursos->count()}}<sup style="font-size: 20px"></sup></h3>
+                            <p>Cursos</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+            </div>
+
+ 
+        </div>
+    </section>
+
+
 @stop
 
 @section('css')
@@ -15,7 +101,7 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script>
+        console.log('Hi!');
+    </script>
 @stop
-
- 
